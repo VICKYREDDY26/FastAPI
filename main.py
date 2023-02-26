2c297ade2a158bcc9dcf3ff84e30c35ac732acfe
VICKYREDDY26 <learningproject143@gmail.com>
2023-02-26 15:16:35 -0600

    added get Posts

--- main.py
+++ main.py
@@ -8,3 +8,26 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+# added the @app.post decorator to define the endpoint  to get posts items.
+@app.get("/items/{item_id}")
+def get_post_items():
+    posts = [
+        {
+            "id": 1,
+            "title": "First Post",
+            "body": "This is the first post"
+        },
+        {
+            "id": 2,
+            "title": "Second Post",
+            "body": "This is the second post"
+        },
+        {
+            "id": 3,
+            "title": "Third Post",
+            "body": "This is the third post"
+        }
+    ]
+    return posts
